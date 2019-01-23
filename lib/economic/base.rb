2eb20d5d8b7e29f2b1f72924e6fb9cddb5896781
Peter Klogborg <klogborg@traels.it>
2019-01-23 13:52:54 +0100

    Bug fix.

--- lib/economic/base.rb
+++ lib/economic/base.rb
@@ -14,12 +14,18 @@ module Economic
       Hash.class_eval { include ExtraMethods }
       @internal_hash = hash
       @internal_hash.each do |k, v|
-        send("#{k}=", v) if self.class::ATTRIBUTES.include?(k)
-        if self.class::OBJECTS.include?(k)
+        k = k.to_s
+        if self.class::ATTRIBUTES.include?(k)
+          warn "#{k} in #{self.class.name} contains a Hash #{self.class.name}" if v.class == Hash
+          send("#{k}=", v)
+        elsif self.class::OBJECTS.include?(k)
+          warn "#{k} in #{self.class.name} does not contain a Hash" unless v.class == Hash
           v.keys.count.times do |i|
             v.alias!(Base.snake_case(v.keys[i]), v.keys[i])
           end
           send("#{k}=", v)
+        else
+          warn "unassigned k #{k} in #{self.class.name}" unless %w[layout self soap metaData].include? k
         end
       end
     end
@@ -50,7 +56,7 @@ module Economic
     end
 
     def self.snake_case(camel_cased)
-      camel_cased.gsub(/::/, '/')
+      camel_cased.to_s.gsub(/::/, '/')
                  .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
                  .gsub(/([a-z\d])([A-Z])/, '\1_\2')
                  .tr('-', '_')

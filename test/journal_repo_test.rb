5df34a53b49f4fa642d9ddda79176f88f9512255
Simon Træls Ravn <cs2@cs2.dk>
2019-02-27 11:08:18 +0100

    Chore. Remove tests that are now in voucher repo.

--- test/journal_repo_test.rb
+++ test/journal_repo_test.rb
@@ -6,7 +6,7 @@ class JournalRepoTest < Minitest::Test
       Economic::Session.authentication('Demo', 'Demo')
     end
 
-    it 'gets all' do
+    it 'can get all journals' do
       stub_get_request(endpoint: 'journals-experimental', fixture_name: 'journals_0')
 
       journals = Economic::JournalRepo.all
@@ -15,7 +15,7 @@ class JournalRepoTest < Minitest::Test
       assert_kind_of Economic::Journal, journals[0]
     end
 
-    it 'finds based on customer number' do
+    it 'can find a specific journal by number' do
       stub_get_request(endpoint: 'journals-experimental', page_or_id: 2, fixture_name: 'journal')
 
       journal = Economic::JournalRepo.find(2)
@@ -25,58 +25,12 @@ class JournalRepoTest < Minitest::Test
     end
 
     it 'returns json data based on changes to the model' do
+      # should be moved, this is basicaly a model test
       stub_get_request(endpoint: 'journals-experimental', page_or_id: 2, fixture_name: 'journal')
 
       journal = Economic::JournalRepo.find(2)
 
       assert journal.to_h.inspect.include? 'Lønninger'
-    end
-
-    it 'gets vouchers on a journal' do
-      stub_get_request(endpoint: 'journals-experimental', page_or_id: 2, fixture_name: 'journal')
-
-      stub_request(:post, 'https://restapi.e-conomic.com/journals-experimental/2/vouchers')
-        .with(
-          body: { "journal": { "journalNumber": 2 }, "entries": { "customerPayments": [{ "customer": { "customerNumber": 1 }, "amount": 100, "date": '2017-05-03' }] }, "accountingYear": { "year": '2017' } }
-        )
-        .to_return(status: 201, body: '[{"accountingYear":{"year":"2017","self":"https://restapi.e-conomic.com/accounting-years/2017"},"journal":{"journalNumber":2,"self":"https://restapi.e-conomic.com/journals-experimental/2"},"entries":{"customerPayments":[{"customer":{"customerNumber":1,"self":"https://restapi.e-conomic.com/customers/1"},"journal":{"journalNumber":2,"self":"https://restapi.e-conomic.com/journals-experimental/2"},"amount":100.00,"currency":{"code":"DKK","self":"https://restapi.e-conomic.com/currencies/DKK"},"date":"2017-05-03","exchangeRate":100.000000,"entryType":"customerPayment","voucher":{"accountingYear":{"year":"2017","self":"https://restapi.e-conomic.com/accounting-years/2017"},"voucherNumber":50026,"attachment":"https://restapi.e-conomic.com/journals-experimental/2/vouchers/2017-50026/attachment","self":"https://restapi.e-conomic.com/journals-experimental/2/vouchers/2017-50026"},"amountDefaultCurrency":100.00,"remainder":100.00,"remainderDefaultCurrency":100.00,"journalEntryNumber":27,"metaData":{"delete":{"description":"Delete this entry.","href":"https://restapi.e-conomic.com/journals-experimental/2/entries/27","httpMethod":"delete"}},"self":"https://restapi.e-conomic.com/journals-experimental/2/entries/27"}]},"voucherNumber":50026,"attachment":"https://restapi.e-conomic.com/journals-experimental/2/vouchers/2017-50026/attachment","self":"https://restapi.e-conomic.com/journals-experimental/2/vouchers/2017-50026"}]', headers: {})
-
-      journal = Economic::JournalRepo.find(2)
-      v = Economic::Voucher.new({})
-      v.entries = { 'customerPayments':
-          [{
-            'customer': {
-              'customerNumber': 1
-            },
-            'amount': 100,
-            'date': '2017-05-03'
-          }] }
-      v.journal = { "journalNumber": 2 }
-      v.accountingYear.year = '2017'
-      assert journal.create_voucher(v)
-    end
-
-    it 'post financeVouchers' do
-      stub_get_request(endpoint: 'journals-experimental', page_or_id: 5, fixture_name: 'journal_5')
-
-      stub_request(:post, 'https://restapi.e-conomic.com/journals-experimental/5/vouchers')
-        .with(
-          body: { "accountingYear": { "year": '2017' }, "journal": { "journalNumber": 5 }, "entries": { "financeVouchers": [{ "contraAccount": { "accountNumber": 1010 }, "amount": 100, "date": '2017-02-01' }] } }
-        ).to_return(status: 201, body: '', headers: {})
-
-      journal = Economic::JournalRepo.find(5)
-      v = Economic::Voucher.new({})
-      v.entries = { 'financeVouchers':
-          [{
-            'contraAccount': {
-              'accountNumber': 1010
-            },
-            'amount': 100,
-            'date': '2017-02-01'
-          }] }
-      v.journal = { "journalNumber": 5 }
-      v.accountingYear.year = '2017'
-      journal.create_voucher(v)
     end
   end
 end

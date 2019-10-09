2a9837f4fd0989a3c6ceb116a47a66c5738a3387
Nicolai Bach Woller <woller@traels.it>
2019-10-09 13:37:15 +0200

    New feature. Book a draft invoice.

--- lib/economic/invoice.rb
+++ lib/economic/invoice.rb
@@ -13,6 +13,7 @@ module Economic
     field :roundingAmount
     field :vatAmount
     field :draftInvoiceNumber, id: true # This changes name depending on its type (draft, unpaid etc)
+    field :bookedInvoiceNumber
 
     relation :customer, fields: [:customerNumber]
     relation :delivery, fields: []
--- lib/economic/invoices/booked_repo.rb
+++ lib/economic/invoices/booked_repo.rb
@@ -1,6 +1,23 @@
 module Economic
   module Invoices
     class BookedRepo < Economic::Invoices::Repo
+      class << self
+        def send(invoice)
+          response = send_request(method: :post, url: URI.escape(endpoint_url), payload: payload(invoice))
+
+          entry_hash = JSON.parse(response.body)
+
+          invoice.class.new(entry_hash)
+        end
+
+        private
+
+        def payload(invoice)
+          {
+            draftInvoice: invoice.to_h,
+          }.to_json
+        end
+      end
     end
   end
 end

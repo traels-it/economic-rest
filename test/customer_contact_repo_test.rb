dbf797021cec292d4168fd41b2cfb8bbe7000096
Nicolai Bach Woller <woller@traels.it>
2019-10-15 09:27:50 +0200

    New feature. Find all customer_contacts on a customer.

--- test/customer_contact_repo_test.rb
+++ test/customer_contact_repo_test.rb
@@ -11,5 +11,17 @@ class CustomerContactRepoTest < Minitest::Test
 
       assert_equal "https://restapi.e-conomic.com/customers/12/contacts", Economic::CustomerContactRepo.endpoint_url(model)
     end
+
+    describe "#all" do
+      it "finds all customer contacts on a customer" do
+        stub_get_request(endpoint: "customers/1/contacts", fixture_name: "customer_contact_0")
+
+        customer = Economic::Customer.new({"name" => "Some customer", "customerNumber" => 1})
+        customer_contacts = Economic::CustomerContactRepo.all(on: customer)
+
+        assert_kind_of Economic::CustomerContact, customer_contacts.first
+        assert_equal "Simon Testersen", customer_contacts.first.name
+      end
+    end
   end
 end

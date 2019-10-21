3367d029693f330b34f45dcf8595a4e536e4b4b7
Nicolai Bach Woller <woller@traels.it>
2019-10-21 12:24:12 +0200

    New feature. Book invoice with supplied number.

--- test/booked_invoice_repo_test.rb
+++ test/booked_invoice_repo_test.rb
@@ -14,5 +14,14 @@ class BookedInvoiceRepoTest < Minitest::Test
       assert_kind_of Economic::Invoice, booked_invoice
       assert_equal 10, booked_invoice.booked_invoice_number
     end
+
+    it "can book an invoice with a specific supplied number" do
+      stub_get_request(endpoint: "invoices/booked", fixture_name: "invoice_book_with_number", method: :post, paged: false)
+
+      booked_invoice = Economic::Invoices::BookedRepo.send(Economic::Invoice.new, book_with_number: 9999999)
+
+      assert_kind_of Economic::Invoice, booked_invoice
+      assert_equal 9999999, booked_invoice.booked_invoice_number
+    end
   end
 end

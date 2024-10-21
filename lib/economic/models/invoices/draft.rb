007cda1648fa5f4cacb5846ff8b71586db5c7f22
Nicolai Bach Woller <woller@traels.it>
2024-10-21 09:47:00 +0200

    Feature. Inherit attributes from parent.

--- lib/economic/models/invoices/draft.rb
+++ lib/economic/models/invoices/draft.rb
@@ -3,6 +3,11 @@ module Economic
     module Invoices
       class Draft < Economic::Models::Invoice
         field :id, as: :draftInvoiceNumber
+
+        # This could be cool to move to Economic::Model
+        Economic::Models::Invoice.attributes.each do |attribute|
+          attributes << attribute
+        end
       end
     end
   end

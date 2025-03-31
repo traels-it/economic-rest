a12ebef2f9bf963a8ec5a5b0744b9de771db1bf2
Nicolai Bach Woller <woller@traels.it>
2025-03-31 12:36:38 +0200

    Fix. Add Pdf repo to make authentication easier and the structure more similar to the rest of the gem.

--- test/repos/pdf_test.rb
+++ test/repos/pdf_test.rb
@@ -1,24 +1,19 @@
 require "test_helper"
 
-class Economic::Models::PdfTest < Minitest::Test
-  describe Economic::Models::Pdf do
-    before { set_credentials }
-
+module Repos
+  class PdfTest < Minitest::Test
     describe "#download_pdf" do
+      before { set_credentials }
+
       it "returns the pdf as a string" do
         stub_request(:get, "https://restapi.e-conomic.com/invoices/booked/9999999/pdf")
           .to_return(status: 200, body: File.read("test/fixtures/pdf/booked_invoice.pdf"))
 
         pdf = Economic::Models::Pdf.new(download: "https://restapi.e-conomic.com/invoices/booked/9999999/pdf")
-        result = pdf.download_pdf
+
+        result = Economic::Repos::Pdf.new.download(pdf)
 
         assert_kind_of String, result
-      end
-
-      it "raises an error, if the pdf has no download url" do
-        assert_raises StandardError do
-          Economic::Models::Pdf.new.download_pdf
-        end
       end
     end
   end

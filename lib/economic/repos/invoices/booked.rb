f570ca4703899dc1c5b62b8517f7f50cfa87074a
Simon Træls Ravn <traels@traels.it>
2025-09-18 08:34:01 +0200

    Fix. Book invoice with number in new repo.

--- lib/economic/repos/invoices/booked.rb
+++ lib/economic/repos/invoices/booked.rb
@@ -2,9 +2,10 @@ module Economic
   module Repos
     module Invoices
       class Booked < Economic::Repo
-        def create(model, send_by: nil)
+        def create(model, send_by: nil, book_with_number: nil)
           hash = model.to_h
           hash[:sendBy] = send_by if send_by.to_s == "ean"
+          hash[:bookWithNumber] = book_with_number if book_with_number.present?
           super(hash)
         end
 
--- test/repos/invoices/booked_test.rb
+++ test/repos/invoices/booked_test.rb
@@ -46,6 +46,24 @@ module Repos
             assert_equal "5798000363007", created_invoice.recipient.ean
             assert_equal 1, created_invoice.recipient.attention.customer_contact_number
           end
+
+          it "is possible to supply number that invoices should be booked under" do
+            stub_request(:post, "https://restapi.e-conomic.com/invoices/booked")
+              .with(
+                body: {draftInvoice: {draftInvoiceNumber: 142}, sendBy: "ean", bookWithNumber: 38}
+              ).to_return(status: 201, body: File.read(json_fixture("create_booked_invoice_with_ean")), headers: {})
+
+            booked_invoice = Economic::Models::Invoices::Booked.new(
+              draft_invoice: Economic::Models::Invoices::Draft.new(
+                id: 142
+              )
+            )
+
+            created_invoice = Economic::Repos::Invoices::Booked.new.create(booked_invoice, send_by: :ean, book_with_number: 38)
+
+            assert_instance_of Economic::Models::Invoices::Booked, created_invoice
+            assert_equal 38, created_invoice.id
+          end
         end
       end
     end
